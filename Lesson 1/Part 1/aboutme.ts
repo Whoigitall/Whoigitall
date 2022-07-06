89135e555223e40fd1fdeab1d22de2e4bd5d156c
Sayat <whoigitall@gmail.com>
2022-07-06 13:41:07 +0600

    Lesson 1 Part 1

---
+++ Lesson 1/Part 1/aboutme.ts
@@ -0,0 +1,22 @@
+const Myname: string = 'Sayat';
+//это мое имя
+const myAge: number = 45; // мой возраст
+const isOlderFortyFive: boolean = true; // да это верно
+const isOlderFortyFour: boolean = false; //это не правда
+const myFavoriteHobby: string = "Diving"; // это мое увлечение(хобби)
+const myMaxAllowableDepthMetter: number = 20; 
+// допуск на погружение
+const iLearning: string = 'JavaScript&TypeScript';
+const progLanguageJSAndTS: boolean = true;
+const progLanguagePythone: boolean = false;
+const itWillTakeMonth: number = 3; //продолжительность обучения
+const yes: boolean = true;
+const non: boolean = false;
+const iLiveIn: string = "inAlmaty";
+const usedToLive: string = 'inTula';
+const livedThereMonths: number=11;
+const livedInAlmaty: number = 3
+const iLike: string = 'Tula&Almaty';
+const iDontLike: string = 'Honey';
+const thisIs: boolean = false;
+const iLikeHoney: boolean = true;
