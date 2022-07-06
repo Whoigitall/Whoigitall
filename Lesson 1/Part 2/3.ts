b981238af74b3ef5e9353525397ac2ff54e65999
Sayat <whoigitall@gmail.com>
2022-07-06 15:07:50 +0600

    Create 3.ts

---
+++ Lesson 1/Part 2/3.ts
@@ -0,0 +1,7 @@
+//Даны стороны прямогульника a и b. Найти его площадь и периметр
+const a: number =4;
+const b: number = 5;
+const s: number = a*b;
+const p: number =2*(a+b);
+console.log(s);
+console.log(p);
