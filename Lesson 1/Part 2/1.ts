77cd588b8a303f998b4c4ed07aa4e1f41b82d670
Sayat <whoigitall@gmail.com>
2022-07-06 14:36:09 +0600

    Lesson 1 Part 1 Exessise 1,2

---
+++ Lesson 1/Part 2/1.ts
@@ -0,0 +1,5 @@
+// Дана сторона квадрата a. Найти его периметр P=4*a
+const a: number = 6;
+const p: number = 4*a;
+console.log(p);
+
---
+++ Lesson 1/Part 2/2.ts
@@ -0,0 +1,4 @@
+//Дана сторона квадрата a. Найти его площадь S=a^2
+const a: number =7;
+const s: number = a**2;
+console.log(s);
