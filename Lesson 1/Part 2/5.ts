983243c4211027ea8eac1c3198ed7fd245a3cf4e
Sayat <whoigitall@gmail.com>
2022-07-06 16:14:15 +0600

    Lesson 1 Part 2 Ex5

---
+++ Lesson 1/Part 2/5.ts
@@ -0,0 +1,6 @@
+//Дана длина ребер куба a. Найти объем куба и площадь его поверхности
+const a: number = 24;
+const v: number = a*a*a;
+console.log(v);
+const s: number = 6*a;
+console.log (s)
---
+++ Lesson 1/Part 2/6.ts
@@ -0,0 +1,9 @@
+/*Даны длины ребер a,b,c прямоугольного параллелепипеда.
+Найти его объем и площадь поверхности*/
+const a: number = 5;
+const b: number = 3;
+const c: number = 4;
+const v: number = a*b*c;
+console.log(v);
+const s: number = 6*v;
+console.log(s);
