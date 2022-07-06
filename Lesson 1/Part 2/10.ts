33802e63a7b01bf7f6c9909d942ee80aa4887a4b
Sayat <whoigitall@gmail.com>
2022-07-06 17:25:01 +0600

    Lesson1 Part 2 Ex10

---
+++ Lesson 1/Part 2/10.ts
@@ -0,0 +1,9 @@
+//Даны два ненулевых числа a и b. 
+//Найти сумму, произведение, разность, частное их квадратов
+const a: number = 1;
+const b: number = 2;
+const sum: number = a**2+b**2;
+const multipi: number = a**2*b**2;
+const division: number = a**2/b**2;
+const diff: number = a**2-b**2;
+
